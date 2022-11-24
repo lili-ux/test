e6308b928daecaa5bd4d5f044cfe1154c1b19842
lili-ux <52767974+lili-ux@users.noreply.github.com>
2022-11-24 20:03:01 +0800

    Create rule_test_ak.py

---
+++ rule_test_ak.py
@@ -0,0 +1,6 @@
+ary on;
+        proxy_pass http://127.0.0.1:8888;
+        proxy_set_header Host $host;
+        VOLC_Pak='TjJRMUUxxxtZVFxxx0dVek5HUXpxxxmd5T0RxxxbE16aGtPREUzWxxxbU5Uaw=='
+
+        proxy_set_header X-Real-IP $remot
