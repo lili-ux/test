0e874bb22908e12c692c3ca22b1282802c873f29
lili-ux <318679913@qq.com>
2023-02-27 17:31:53 +0800

    test2 commit

--- rule_test_auth.py
+++ rule_test_auth.py
@@ -4,6 +4,7 @@ gzip_min_length 1k;
         gzip_comp_level 9;
         gzip_types text/plain t
   auth='TjJRMUUxxxtZVFxxx0dVek5HUXpxxxmd5T0RxxxbE16aGtPREUzWxxxbU5Uaw=='
+ auth='TjrJRMUUxxxtZVFxxx0dVek5HUXpxxxmd5T0RxxxbE16aGtPREUzWxxxbU5Uaw=='
         ext/javascript application/javascript image/jpeg image/gif image/png application/font-woff application/x-javascript text/css application/xml;
         gzip_vary on;
         root /Hawkeye/client/dist;
