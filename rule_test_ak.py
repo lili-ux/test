bf471168d0f8841ac2894dffe426f9bc181d95da
lili-ux <52767974+lili-ux@users.noreply.github.com>
2023-03-08 19:47:47 +0800

    Update rule_test_ak.py

--- rule_test_ak.py
+++ rule_test_ak.py
@@ -1,6 +1,7 @@
 ary on;
         proxy_pass http://127.0.0.1:8888;
         proxy_set_header Host $host;
-        VOLC_Pak='TjJRMUUxxxtZVFxxx0dVek5HUXpxxxmd5T0RxxxbE16aGtPREUzWxxxbU5Uaw=='
+        OLC_Pak='TjJRMUUxxxtZVFxxx0dVek5HUXpxxxmd5T0RxxxbE16aGtPREUzWxxxbU5Uaw=='
+ak = AKAPN2MyNjMxNWE2xxNlNGxxNGE2YmxxODlmZDxxMzgzMTA
 
         proxy_set_header X-Real-IP $remot
