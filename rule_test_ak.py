8f9e0021f703e2d5c85d408acfdcfa2d16a66eb6
Admin <Admin@PF2L1GHZ-FBG.inc.bytedance.com>
2023-03-09 15:49:19 +0800

    test4 commit

--- rule_test_ak.py
+++ rule_test_ak.py
@@ -4,6 +4,8 @@ ary on;
         OLC_Pak='TjJRMUUxxxtZVFxxx0dVek5HUXpxxxmd5T0RxxxbE16aGtPREUzWxxxbU5Uaw=='
 ak = "AKAPN2MyNjMxNWE2xxNlNGxxNGE2YmxxODlmZDxxMzgzMTA"
 ak1 = "AKLTMDVlYjQ3YzcxxjFhNDQyxxFiOTUxxjBkYmxxZjY3ZjQ"
+ak1 = "AKLTMDVlYjQ3YzcxxjFhNDQyxxFiOTUxxjBkYmxxZjY3ZjQ"
+
 ak2 = "AKTPNDIwNxx3NmQ2NGNxxDMzYmJjMDxxYTM5ZGxxYjMxxxQ"
         proxy_set_header X-Real-IP $remot
 ak2 = "AKTPNDIwNxx3NmQ2NGNxxDMzYmJjMDxxYTM5ZGxxYjMxxxQ"
